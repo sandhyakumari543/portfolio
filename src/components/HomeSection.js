b2da79a97b0a899743bc71376dfba4a94854ce71
Sandhya <sandhyakushwaha543@gmai.com>
2024-01-30 00:42:23 +0530

    add in homeSection

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -20,7 +20,7 @@ const HomeSection = () => {
                     <div className="home-info padd-15">
                         <h3 className="hello">Hello, My name is <span className='name'>Sandhya Kumari</span></h3>
                         <h3 className='my-proffesion'>I'm a <spn className="typing"><TypingAnimation /></spn></h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime cupiditate qui molestias!</p>
+                        <p> passionate  having an experience or building some Project with javaScript /Ract.js /Html / Css/ dot Net some other cool libraries and frameworks and I love creating beautiful and responsive web experiences that leave a lasting .</p>
                         <a href="#" className="btn" onClick={downloadResume}>Download CV</a>
                     </div>
                     <div className="home-img padd-15">
